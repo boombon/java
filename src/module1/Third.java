94915ae3b2094bc98857e0ca6c149436c2d76760
Бумбон <bumbon@iMac.local>
2016-09-12 23:14:35 +0300

    module1

---
+++ src/module1/Third.java
@@ -0,0 +1,10 @@
+package module1;
+
+/**
+ * Created by bumbon on 05.09.16.
+ */
+public class Third {
+    public static void main(String[] args) {
+        System.out.print("My first program on JAVA...");
+    }
+}
